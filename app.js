b6865c0515c3de2bb6dc268932717e245806773f
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-11-11 22:39:13 -0600

    added ejs

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+app.set('views','./src/views');
 
-app.use(express.static('src/views'));
+
+
+app.set('view engine', 'ejs');
+
+
+
 
 app.get('/', function(req, res) {
-   res.send('Hello World');
+   res.render('index', {title: 'Hello from render', list:['a', 'b']});
 });
 
 app.get('/books', function(req, res) {
