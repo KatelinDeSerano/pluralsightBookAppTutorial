0adf6c954659a258fd62f76da10f2c9f601ae424
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2017-11-11 23:35:39 -0600

    add Books Router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views','./src/views');
@@ -13,9 +14,28 @@ app.set('view engine', 'ejs');
 
 
 
+bookRouter.route('/')
+    .get(function (req, res) {
+        res.send('Hello Books');
+});
+
+bookRouter.route('/single')
+    .get(function (req, res) {
+        res.send('Hello Single Book');
+});
+
+app.use('/Books', bookRouter);
 
 app.get('/', function(req, res) {
-   res.render('index', {title: 'Hello from render', list:['a', 'b']});
+   res.render('index', {title: 'Hello from render', 
+                        nav: [{
+                                Link:'/Books', 
+                                Text:'Books'
+                              }, {
+                                Link: '/Authors', 
+                                Text:'Authors'
+                              }]
+                       });
 });
 
 app.get('/books', function(req, res) {
